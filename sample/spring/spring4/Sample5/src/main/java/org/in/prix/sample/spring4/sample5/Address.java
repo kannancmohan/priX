8e75bbd490437a28e3c87c59f390a65544594988
kannancmohan <kannanmohanklm@gmail.com>
2014-06-18 23:36:06 +0530

    Update Sample5 for changing to setter based injection Change-Id: Iac05a1cfe740d0b54bb04e41024bb0eca2caac30 Signed-off-by: kannancmohan <kannanmohanklm@gmail.com>

--- sample/spring/spring4/Sample5/src/main/java/org/in/prix/sample/spring4/sample5/Address.java
+++ sample/spring/spring4/Sample5/src/main/java/org/in/prix/sample/spring4/sample5/Address.java
@@ -2,18 +2,36 @@ package org.in.prix.sample.spring4.sample5;
 
 public class Address {
 
-    private final String address1;
-    private final String address2;
-    private final String street;
-
-    public Address(String address1, String address2, String street) {
-        this.address1 = address1;
-        this.address2 = address2;
-        this.street = street;
-    }
+    private String address1;
+    private String address2;
+    private String street;
 
     public String getAddress() {
         return "Address [address1=" + address1 + ", address2=" + address2 + ", street=" + street + "]";
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
     }
 
 }
